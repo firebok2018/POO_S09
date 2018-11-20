113d777e38c6781b5403caa7009596ee85689603
unknown <SUITE@SUITESA102-ST07.cibertec.edu.pe>
2018-11-20 12:21:56 -0500

    CRUD con Trabnsaciones

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConec = new System.Windows.Forms.Button();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.Eliminar = new System.Windows.Forms.Button();
+            this.dgpais = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgpais)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConec
+            // 
+            this.btnConec.Location = new System.Drawing.Point(68, 148);
+            this.btnConec.Name = "btnConec";
+            this.btnConec.Size = new System.Drawing.Size(75, 23);
+            this.btnConec.TabIndex = 0;
+            this.btnConec.Text = "Conectar";
+            this.btnConec.UseVisualStyleBackColor = true;
+            this.btnConec.Click += new System.EventHandler(this.btnConec_Click);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(112, 52);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(112, 78);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(68, 192);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(68, 221);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 4;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.Location = new System.Drawing.Point(68, 267);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(75, 23);
+            this.Eliminar.TabIndex = 5;
+            this.Eliminar.Text = "Eliminar";
+            this.Eliminar.UseVisualStyleBackColor = true;
+            this.Eliminar.Click += new System.EventHandler(this.Eliminar_Click);
+            // 
+            // dgpais
+            // 
+            this.dgpais.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgpais.Location = new System.Drawing.Point(257, 52);
+            this.dgpais.Name = "dgpais";
+            this.dgpais.Size = new System.Drawing.Size(515, 384);
+            this.dgpais.TabIndex = 6;
+            // 
+            // Codigo
+            // 
+            this.Codigo.AutoSize = true;
+            this.Codigo.Location = new System.Drawing.Point(36, 59);
+            this.Codigo.Name = "Codigo";
+            this.Codigo.Size = new System.Drawing.Size(43, 13);
+            this.Codigo.TabIndex = 7;
+            this.Codigo.Text = "Codigo:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nombre:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Codigo);
+            this.Controls.Add(this.dgpais);
+            this.Controls.Add(this.Eliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.btnConec);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgpais)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConec;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button Eliminar;
+        private System.Windows.Forms.DataGridView dgpais;
+        private System.Windows.Forms.Label Codigo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConec_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            using (SqlTransaction tr = con.BeginTransaction(IsolationLevel.Serializable))
+            {
+                SqlCommand cmd = new SqlCommand("usp_addpais", con,tr);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@idpais", txtCodigo.Text);
+                cmd.Parameters.AddWithValue("@nombrepais", txtNombre.Text);
+                try
+                {
+                    int i = cmd.ExecuteNonQuery();
+                    tr.Commit();
+                    MessageBox.Show(" pais agregasdo " + i);
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex + "");
+                    tr.Rollback();
+                   
+                }
+
+
+            }
+            con.Close();
+            dgpais.DataSource = null;
+            dgpais.DataSource = listaso();
+        }
+        public DataTable listaso()
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("usp_pais", con);
+            da.Fill(dt);
+            return dt;
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            try
+            {
+                using (SqlTransaction tr = con.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    SqlCommand cmd = new SqlCommand("usp_updatepais", con,tr);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@idpais", txtCodigo.Text);
+                    cmd.Parameters.AddWithValue("@nombrepais", txtNombre.Text);
+                    cmd.ExecuteNonQuery();
+                    try
+                    {
+                        tr.Commit();
+                        MessageBox.Show(" pais MOdificado ");
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                        tr.Rollback();
+                    }
+                }   
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+            dgpais.DataSource = null;
+            dgpais.DataSource = listaso();
+        }
+
+        private void Eliminar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            try
+            {
+                using (SqlTransaction tr = con.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    SqlCommand cmd = new SqlCommand("usp_deletepais", con, tr);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@idpais", txtCodigo.Text);
+                 
+                    cmd.ExecuteNonQuery();
+                    try
+                    {
+                        tr.Commit();
+                        MessageBox.Show(" pais Eliminado ");
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                        tr.Rollback();
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+            dgpais.DataSource = null;
+            dgpais.DataSource = listaso();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgpais.DataSource = listaso();
+        }
+    }
+   
+}
